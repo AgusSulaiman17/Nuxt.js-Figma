cc0d0156dd54c3e5495232e5d5fed9e4590ac1dd
AgusSulaiman17 <agus.sulaiman172006@gmail.com>
2024-07-19 13:18:47 +0700

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,25 @@
+<template>
+  <body>
+    <div class="container">
+    <AppHeader />
+    <NuxtRouteAnnouncer />
+    <AppLanding />
+    <AppLogotypes />
+    <AppSubheading />
+    <AppCard />
+  </div>
+  </body>
+</template>
+<style>
+body {
+  margin: 0;
+  width: 100%;
+  justify-content: center;
+  display: inline-flex;
+  padding: 0;
+  box-sizing: border-box;
+}
+.container{
+  width: 1443px;
+}
+</style>
